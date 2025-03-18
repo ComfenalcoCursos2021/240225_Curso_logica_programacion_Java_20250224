5b37eaf7bc60836ad3a9826bb126115c8510cf7e
alvaro.obregon <alvaro911>
2025-03-17 20:48:02 -0500

    Clase 20250317 p1

---
+++ WORKSPACE/DobleDeUnNumero/src/main/java/aoc/fundamentos/febrero/dobledeunnumero/DobleDeUnNumero.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package aoc.fundamentos.febrero.dobledeunnumero;
+
+/**
+ *
+ * @author DELL
+ */
+public class DobleDeUnNumero {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
