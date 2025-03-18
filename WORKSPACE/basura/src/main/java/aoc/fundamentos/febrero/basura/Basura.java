47f9276513def24e07c530ad76b603bba2637727
alvaro.obregon <alvaro911>
2025-03-17 21:08:31 -0500

    Clase 20250317 p1

---
+++ WORKSPACE/basura/src/main/java/aoc/fundamentos/febrero/basura/Basura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package aoc.fundamentos.febrero.basura;
+
+/**
+ *
+ * @author DELL
+ */
+public class Basura {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
