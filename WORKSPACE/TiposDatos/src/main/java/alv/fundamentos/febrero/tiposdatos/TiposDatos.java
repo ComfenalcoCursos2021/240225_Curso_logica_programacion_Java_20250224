4dcda00d2331eab08f1f50bcbbc4e806a0396e6f
alvaro.obregon <alvaro911>
2025-03-17 20:30:57 -0500

    clase 20250317

---
+++ WORKSPACE/TiposDatos/src/main/java/alv/fundamentos/febrero/tiposdatos/TiposDatos.java
@@ -0,0 +1,30 @@
+package alv.fundamentos.febrero.tiposdatos;
+
+public class TiposDatos {
+
+    public static void main(String[] args) {
+        byte a = 1;
+        System.out.println("BYTE MAX-> "+Byte.MAX_VALUE);
+        System.out.println("BYTE MIN-> "+Byte.MIN_VALUE);
+        
+        short b = 1;
+        System.out.println("SHORT MAX-> "+Short.MAX_VALUE);
+        System.out.println("SHORT MIN-> "+Short.MIN_VALUE);
+        
+        int c = 1;
+        System.out.println("INTEGER MAX-> "+Integer.MAX_VALUE);
+        System.out.println("INTEGER MIN-> "+Integer.MIN_VALUE);
+        
+        long d = 1;
+        System.out.println("LONG MAX-> "+Long.MAX_VALUE);
+        System.out.println("LONG MIN-> "+Long.MIN_VALUE);
+        
+        float g = 1.0f;
+        System.out.println("FLOAT MAX-> "+Float.MAX_VALUE);
+        System.out.println("FLOAT MIN-> "+Float.MIN_VALUE);
+        
+        double h = 1.0;
+        System.out.println("DOUBLE MAX-> "+Double.MAX_VALUE);
+        System.out.println("DOUBLE MIN-> "+Double.MIN_VALUE);
+    }
+}
---
+++ WORKSPACE/TiposDatosVsCode/src/aoc/fund/tipos/datos/App.java
@@ -0,0 +1,7 @@
+package aoc.fund.tipos.datos;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
